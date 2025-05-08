917109fa2507e3960c887a05f8ca81333e4d7721
KristofferKristiansen <mattiskk@hiof.no>
2025-05-08 12:46:16 +0200

    la inn prosjektid

--- my-sanity-project/sanity.cli.js
+++ my-sanity-project/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '23xgcjk9',
+    projectId: '8yvg7r7t',
     dataset: 'production'
   },
   /**
