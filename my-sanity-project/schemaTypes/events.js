b8b7b79834fda2739937fb2450a97c148135142b
KristofferKristiansen <mattiskk@hiof.no>
2025-05-15 13:23:04 +0200

    fikset events for sanity

--- my-sanity-project/schemaTypes/events.js
+++ my-sanity-project/schemaTypes/events.js
@@ -1,69 +1,26 @@
-export const events = {
-    name: 'events',
-    title: 'events',
-    type: 'document',
-    apild: '',
-    fields: [
-        {
-            name: 'events1',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events2',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events3',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events4',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events5',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events6',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events7',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events8',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events9',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-        {
-            name: 'events10',
-            title: 'events',
-            apild: '',
-            type: 'string'
-        },
-
-    ]
-}
+export const event = {
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Tittel på arrangementet',
+      type: 'string'
+    },
+    {
+      name: 'apiId',
+      title: 'API-ID',
+      type: 'string',
+      description: 'ID hentet fra Ticketmaster sitt API'
+    },
+    {
+      name: 'category',
+      title: 'Kategori',
+      type: 'string',
+      options: {
+        list: ['Sport', 'Show', 'Festival']
+      }
+    }
+  ]
+};
