1056a69e6d45315cb2a0d7bb71652ca540a36952
KristofferKristiansen <mattiskk@hiof.no>
2025-05-15 13:06:53 +0200

    fikset sanity

---
+++ my-sanity-project/schemaTypes/events.js
@@ -0,0 +1,69 @@
+export const events = {
+    name: 'events',
+    title: 'events',
+    type: 'document',
+    apild: '',
+    fields: [
+        {
+            name: 'events1',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events2',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events3',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events4',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events5',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events6',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events7',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events8',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events9',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+        {
+            name: 'events10',
+            title: 'events',
+            apild: '',
+            type: 'string'
+        },
+
+    ]
+}
