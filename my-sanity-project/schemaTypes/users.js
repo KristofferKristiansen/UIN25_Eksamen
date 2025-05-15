17d981ad03de8d8aa8a23e9173015bf5ea36efe4
KristofferKristiansen <mattiskk@hiof.no>
2025-05-15 13:28:18 +0200

    oppdaterte users for sanity

--- my-sanity-project/schemaTypes/users.js
+++ my-sanity-project/schemaTypes/users.js
@@ -1,15 +1,37 @@
 export const users = {
-    name: "users",
-    title: "users",
-    type: "document",
-    fields:[
+  name: 'user',
+  title: 'Bruker',
+  type: 'document',
+  fields: [
     {
-        name: "name",
-        gender: "gender",
-        age: "age",
-        previousPurchases: "event",
-        wishlist: "event",
-        type: "string"
+      name: 'name',
+      title: 'Navn',
+      type: 'string'
+    },
+    {
+      name: 'gender',
+      title: 'Kjønn',
+      type: 'string',
+      options: {
+        list: ['Mann', 'Kvinne', 'Annet']
+      }
+    },
+    {
+      name: 'age',
+      title: 'Alder',
+      type: 'number'
+    },
+    {
+      name: 'previousPurchases',
+      title: 'Tidligere kjøp',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'event' }] }]
+    },
+    {
+      name: 'wishlist',
+      title: 'Ønskeliste',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'event' }] }]
     }
-    ]
-}
+  ]
+};
